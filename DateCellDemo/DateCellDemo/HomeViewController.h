a7b8f44b15383315c513a486ff94a892e9faee4c
rookie250 <zhou@jeffzhou.local>
2014-05-25 23:06:19 +0800

    初始化一个UITableViewController和UINavigationController

---
+++ DateCellDemo/DateCellDemo/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  DateCellDemo
+//
+//  Created by jeff.zhou on 14-5-25.
+//  Copyright (c) 2014年 rooike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UITableViewController
+
+@end
---
+++ DateCellDemo/DateCellDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DateCellDemo
+//
+//  Created by jeff.zhou on 14-5-25.
+//  Copyright (c) 2014年 rooike. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZAppDelegate class]));
+    }
+}
